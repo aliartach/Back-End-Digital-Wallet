fb531606c0084e58fbc97f0dc4b0253a29ef1aff
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-11-22 13:00:59 +0200

    User (Model/Controller/Routes)

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-import express from  "express";
-import dotenv from 'dotenv';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import sequelize from "./config/database.js";
+//Routes
+import userRoutes from "./routes/UserRoutes.js";
 
 const app = express();
-
+dotenv.config();
 
 // Middleware
 app.use(express.json());
-dotenv.config();
+app.use(cors());
+app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+  // res.header("Access-Control-Max-Age", "3600");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token"
+  // );
+  console.log(req.path, req.method);
+  next();
+});
 
-//listen to port 
+app.use("/api", userRoutes);
+//listen to port
 app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-  });
+  console.log("listening on port", process.env.PORT);
+});
