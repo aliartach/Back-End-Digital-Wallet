63c3a7b2e2813efa48f1d4ab89e4d5592982e6ba
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-12-07 11:59:36 +0200

    Transaction Create Fix

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,6 +1,7 @@
-import { User , Promotion , Transaction} from "../models/index.js"
+import { Transaction, User, Promotion } from "../models/index.js";
+import sequelize from "../config/database.js";
 
-
+// Get all transactions
 export const getAllTransactions = async (req, res) => {
   try {
     const transactions = await Transaction.findAll({
@@ -38,30 +39,76 @@ export const getTransactionById = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch transaction" });
   }
 };
+
+// Create a new transaction
 export const createTransaction = async (req, res) => {
+  const { amount, date, moneyType, senderId, receiverId, promoCode } = req.body;
+  
+  console.log(req.body)
+
   try {
-    const { amount, date, moneyType } = req.body;
+    const transaction = await sequelize.transaction(async (t) => {
+      const [sender, receiver] = await Promise.all([
+        User.findByPk(senderId, { transaction: t }),
+        User.findByPk(receiverId, { transaction: t }),
+      ]);
 
-   
-    const sender = await User.findByPk(senderId);
-    const receiver = await User.findByPk(receiverId);
-
-    if (!sender || !receiver) {
-      return res.status(404).json({ error: 'Sender or receiver not found' });
-    }
-
-    if (promotionsId) {
-      const promotion = await Promotion.findByPk(promotionsId);
-      if (!promotion) {
-        return res.status(404).json({ error: 'Promotion not found' });
+      if (!sender || !receiver) {
+        return res.status(404).json({ error: "Invalid sender or receiver" });
       }
-    }
 
-    // Create transaction
-    const transaction = await Transaction.create({
-      amount,
-      date,
-      moneyType: moneyType.toUpperCase()
+      let promotion = null;
+      let discount = 0;
+      let newAmount = amount;
+
+      if (promoCode) {
+        promotion = await Promotion.findOne({
+          where: { promoCode: promoCode },
+          transaction: t,
+        });
+
+        if (!promotion) {
+          return res.status(404).json({ error: "Invalid promotion" });
+        }
+
+        discount = promotion.percentage;
+        newAmount = amount - (amount * discount) / 100;
+      }
+
+      if (moneyType === "usd") {
+        if (sender.balanceUSD < amount) {
+          return res.status(400).json({ error: "Insufficient funds" });
+        }
+
+        await sender.decrement("balanceUSD", { by: newAmount, transaction: t });
+        await receiver.increment("balanceUSD", {
+          by: newAmount,
+          transaction: t,
+        });
+        await receiver.decrement("balanceUSDT", { by: amount, transaction: t });
+        await sender.increment("balanceUSDT", { by: amount, transaction: t });
+      } else if (moneyType === "usdt") {
+        if (sender.balanceUSDT < amount) {
+          return res.status(400).json({ error: "Insufficient funds" });
+        }
+
+        await sender.decrement("balanceUSDT", { by: amount, transaction: t });
+        await receiver.increment("balanceUSDT", { by: amount, transaction: t });
+      } else {
+        return res.status(400).json({ error: "Invalid money type" });
+      }
+
+      await Transaction.create(
+        {
+          amount: newAmount,
+          date,
+          moneyType,
+          senderId,
+          receiverId,
+          promotionId: promoCode ? promotion.id : null,
+        },
+        { transaction: t }
+      );
     });
 
     return res.status(200).json({ success: true, message: "Money sent successfully" });
@@ -89,28 +136,29 @@ export const updateTransaction = async (req, res) => {
     }
 
     const sender = await User.findByPk(senderId);
+    if (senderId && !sender) {
+      return res.status(404).json({ error: "Sender not found" });
+    }
+
     const receiver = await User.findByPk(receiverId);
-
-    if (!sender || !receiver) {
-      return res.status(404).json({ error: 'Sender or receiver not found' });
+    if (receiverId && !receiver) {
+      return res.status(404).json({ error: "Receiver not found" });
     }
 
-
-    if (promotionsId) {
-      const promotion = await Promotion.findByPk(promotionsId);
-      if (!promotion) {
-        return res.status(404).json({ error: 'Promotion not found' });
-      }
+    const promotion = await Promotion.findByPk(promotionId);
+    if (promotionId && !promotion) {
+      return res.status(404).json({ error: "Promotion not found" });
     }
 
+    transaction.amount = amount;
+    transaction.date = date;
+    transaction.moneyType = moneyType;
+    transaction.senderId = senderId;
+    transaction.receiverId = receiverId;
+    transaction.promotionId = promotionId;
 
-    await transaction.update({
-      amount,
-      date,
-      moneyType: moneyType.toUpperCase()
-    });
-
-    return res.status(200).json(transaction);
+    await transaction.save();
+    res.json(transaction);
   } catch (error) {
     console.error("Failed to update transaction:", error);
     res.status(500).json({ error: "Failed to update transaction" });
