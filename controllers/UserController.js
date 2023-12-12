eea5ead92dbd2a1509a3414abb76d2df3444f17e
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-12-12 04:14:08 +0200

    Socket.Io

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,10 +2,108 @@ import { User, Promotion, Notification, Transaction } from "../models/index.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+import {
+  getPendingNotifications,
+  clearPendingNotifications,
+  storePendingNotification,
+} from "../middleware/socket.js";
+
+import { Server } from "socket.io";
+
+export const io = new Server({
+  cors: { origin: "http://localhost:5173" },
+});
+
+let onlineUsers = [];
+let sharedSocket;
+var sendOfflineUserNotifications;
+
+const addNewUser = (email, socketId) => {
+  !onlineUsers.some((user) => user.email === email) &&
+    onlineUsers.push({ email, socketId });
+};
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+const getUser = (email) => {
+  return onlineUsers.find((user) => user.email === email);
+};
+
+io.on("connection", (socket) => {
+  sharedSocket = socket;
+  socket.on("newUser", (email) => {
+    addNewUser(email, socket.id);
+    console.log("socket: " + socket);
+    console.log("id: " + socket?.id);
+    console.log("userEmail: " + email);
+    onlineUsers.forEach((each) => {
+      console.log("---", each.email);
+    });
+  });
+  socket.on(
+    "sendNotification",
+    ({ senderName, receiverName, amount, time, moneyType }) => {
+      const receiver = getUser(receiverName);
+      if (receiver) {
+        io.to(receiver.socketId).emit("getNotification", {
+          senderName,
+          amount,
+          time,
+          moneyType,
+        });
+        console.log("sendDirectly to:", receiver.email);
+      } else {
+        storePendingNotification(receiverName, {
+          senderName,
+          amount,
+          time,
+          moneyType,
+        });
+      }
+    }
+  );
+
+  sendOfflineUserNotifications = async (email) => {
+    console.log("Socket connected:", socket.connected);
+
+    const pendingNotifications = await getPendingNotifications(email);
+    console.log(
+      "Number of pending notifications:",
+      pendingNotifications.length
+    );
+
+    if (socket.connected && pendingNotifications.length > 0) {
+      setTimeout(() => {
+        pendingNotifications.forEach((notification) => {
+          console.log("storedNFioID:", socket.id);
+
+          io.to(socket.id).emit("getNotification", notification);
+
+          console.log("storedNFio:", notification);
+        });
+      }, 20);
+
+      clearPendingNotifications(email);
+    }
+  };
+
+  socket.on("delUser", () => {
+    removeUser(socket.id);
+    console.log("delUser");
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log("disconnect");
+  });
+});
+
+io.listen(5000);
+
 // Get all users
 const getUsers = async (req, res) => {
   try {
-    const users = await User.findAll({ order: [["id", "DESC"]]});
+    const users = await User.findAll({ order: [["id", "DESC"]] });
     res.json(users);
   } catch (error) {
     res.status(500).json(error.message);
@@ -90,16 +188,28 @@ const signInUser = async (req, res) => {
     }
 
     // Authenticate user with jwt
-    const token = jwt.sign({ id: user.id ,role:user.role,email:user.email,verified:user.verified,firstName:user.firstName}, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRATION,
-    });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+        email: user.email,
+        verified: user.verified,
+        firstName: user.firstName,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRATION,
+      }
+    );
+
+    sendOfflineUserNotifications(user.email);
 
     res.status(200).json({
       success: true,
       email: user.email,
       accessToken: token,
-      role:user.role,
-      verified: user.verified
+      role: user.role,
+      verified: user.verified,
     });
   } catch (err) {
     return res.status(500).json({ error: err.message });
